cd297db8065268f369379ca1e8ff6441a20776e7
itskaustubh <kaustubhpb@gmail.com>
2021-06-05 05:51:55 +0530

    first deploy attempt

--- src/router/index.js
+++ src/router/index.js
@@ -7,21 +7,30 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Browse Warehouses',
+    name: 'Browse',
     component: Home
   },
   {
     path: '/warehouse-:id',
-    name: 'Warehouse Details',
+    name: 'Details',
     component: () => import(/* webpackChunkName: "about" */ '../views/Warehouse.vue'),
     meta: { transitionName: 'slide' },
-  }
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, _, next) => {
+  const possibleRoutes = routes.map(route => route.name)
+  if(possibleRoutes.includes(to.name)){
+    next()
+  }else{
+    next({ name: 'Browse' })
+  }
 })
 
 export default router
